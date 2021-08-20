7dab6e7d8a353e59d5a70243415f657eef1b1785
angelolocritani <67960642+angelolocritani@users.noreply.github.com>
2021-08-20 15:01:32 +0200

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Version](https://img.shields.io/badge/Rimworld-1.3-green.svg)](http://rimworldgame.com/)
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/angelolocritani/Rimworld-RenamePen)](https://github.com/angelolocritani/Rimworld-RenamePen/releases/latest)
+
+# Rimworld-RenamePen
+ A small mod that lets you rename a pen
+ 
+ 
---
+++ Source/RenamePen/RenamePen.cs
@@ -0,0 +1,40 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace RenamePen
+{
+    public class RenamePenMod : Mod
+    {
+        public RenamePenMod(ModContentPack content) : base(content)
+        {
+
+        }
+    }
+
+    public class RenamablePenComp : ThingComp
+    {
+
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            Command_Action command_Action = new Command_Action();
+            command_Action.icon = ContentFinder<Texture2D>.Get("UI/Buttons/Rename");
+            command_Action.defaultLabel = "RenameAnimalPen".Translate();
+            command_Action.defaultDesc = "RenamePen.RenameAnimalPenDesc".Translate();
+            command_Action.action = delegate
+            {
+                Dialog_RenameAnimalPen dialog_RenamePen = new Dialog_RenameAnimalPen(parent.Map, parent.GetComp<CompAnimalPenMarker>());
+                if (KeyBindingDefOf.Misc1.IsDown)
+                {
+                    dialog_RenamePen.WasOpenedByHotkey();
+                }
+                Find.WindowStack.Add(dialog_RenamePen);
+            };
+            command_Action.hotKey = KeyBindingDefOf.Misc1;
+            yield return command_Action;
+
+        }
+    }
+
+}
